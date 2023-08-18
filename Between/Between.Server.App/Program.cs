c71ae996d18c97d1e7b3ea092b967b8c618a0fc7
sjjeon <sjjeon@feelanet.com>
2023-08-18 17:13:34 +0900

    푸시 테스트

--- Between/Between.Server.App/Program.cs
+++ Between/Between.Server.App/Program.cs
@@ -24,17 +24,6 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-
-
-
-
-
-
-
-
-
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
