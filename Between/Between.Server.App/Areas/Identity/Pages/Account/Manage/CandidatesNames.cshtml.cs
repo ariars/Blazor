6396cbc6ef955cf0a654ac4ad2e65f1196188307
ariars <ariars@hotmail.com>
2023-08-21 09:27:42 +0900

    CandidatesNames 컴포넌트 추가

---
+++ Between/Between.Server.App/Areas/Identity/Pages/Account/Manage/CandidatesNames.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Between.Server.App.Areas.Identity.Pages.Account.Manage
+{
+    public class CandidatesNamesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
